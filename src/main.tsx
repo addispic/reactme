2d47e29bdae9f272f6c6940f1fcbcfba9ae790dd
addispic <apic.adisiefenta@gmail.com>
2026-01-30 06:01:07 -0800

    start from zero

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
 
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
-import {Provider} from 'react-redux'
-
-import './index.css'
-import {store} from './store.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-
+  
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  </Provider>
+  
 )

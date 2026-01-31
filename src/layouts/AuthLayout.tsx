4138db72bb4da905442e48a0ac52608608842ce8
addispic <apic.adisiefenta@gmail.com>
2026-01-31 04:04:15 -0800

    Auth is Justs Started

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 export default function AuthLayouts() {
   return (
-    <div>
+    <div className="w-screen h-screen overflow-hidden flex items-center justify-center bg-neutral-50/35">
       <Outlet />
     </div>
   );
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,3 +1,12 @@
 export default function Login() {
-  return <>Login</>;
+  return (
+    <div className="min-w-64 md:min-w-72 p-3 sm:p-4 md:p-5 lg:p-6 rounded-sm sm:rounded-md md:rounded-lg shadow-sm bg-white">
+      <header>
+        <h3 className="sm:text-lg md:text-xl font-medium text-green-600">
+          Login
+        </h3>
+      </header>
+      <div>form</div>
+    </div>
+  );
 }
